08e1e1622acf7f450578c8b19b2f5bbdc65b9d1d
Aleksandr Seleznyov <aleksandr.seleznyov@dxc.com>
2022-08-29 21:04:31 +0200

    Migrate to proper oci auth mechanizm

--- terraform/configuration.tf
+++ terraform/configuration.tf
@@ -2,14 +2,16 @@
 terraform {
   required_version = ">= 1.2.0"
   required_providers {
-    oci = ">= 4.80"
+    oci = {
+      version = ">= 4.90"
+      source  = "oracle/oci"
+    }
   }
 }
 
 provider "oci" {
-  auth                = "SecurityToken"
-  config_file_profile = "DEFAULT"
-  region              = "eu-frankfurt-1"
+  region = "eu-frankfurt-1"
+
 }
 
 
@@ -20,6 +22,8 @@ resource "oci_identity_compartment" "tf-compartment" {
 
 locals {
   vcn_cidr_block                = "10.0.0.0/16"
+  public_subnet_cidr_block      = "10.0.10.0/24"
+  private_subnet_cidr_block     = "10.0.20.0/24"
   oracle_linux_8_amd64_image_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaandftbicox5dje7ufgporxov4o3wckbu5mxw27tyjxolekjwrcgsq"
   oracle_linux_8_arm64_image_id = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaanbxc5ihgflyoyfe7bzohdkovitufny4lyc5hn7wodgao44ljs27a"
 
